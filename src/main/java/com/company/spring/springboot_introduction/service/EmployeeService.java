798474c551be24552e973b88effcdae46b70c75d
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-02-12 18:17:09 +0400

    Add classes from earlier created REST-application

---
+++ src/main/java/com/company/spring/springboot_introduction/service/EmployeeService.java
@@ -0,0 +1,16 @@
+package com.company.spring.springboot_introduction.service;
+
+import com.company.spring.springboot_introduction.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+    public void saveEmployee(Employee employee);
+
+    public Employee getEmployee(int id);
+
+    public void deleteEmployee(int id);
+}
