f0253c69100c216f23674363b4b26bd76dba9e0d
MikhailChrn <mikhail.cherneyev@yandex.ru>
2023-02-12 13:38:18 +0400

    Creating Springboot application using IDEA-Spring-Initializr

---
+++ src/test/java/com/company/spring/springboot_introduction/SpringbootIntroductionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.spring.springboot_introduction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootIntroductionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
